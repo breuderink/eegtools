5e2e4129f35bedd3d11e528883cfcf770a028204
Boris Reuderink <b.reuderink@gmail.com>
2011-08-12 23:34:38 +0200

    Removed accidentally added README.html.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+  name = 'EEGtools',
+  version = '0.1dev',
+  packages = ['eegtools'],
+  licese = 'BSD',
+  long_description = open('README.markdown').read(),
+  )
