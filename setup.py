879033ca031d5b3b2742acd97a0d5ff30506fdf3
Boris Reuderink <b.reuderink@gmail.com>
2012-03-20 20:19:14 +0100

    Finalized 0.2 release.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
   author='Boris Reuderink',
   author_email='b.reuderink@gmail.com',
   license='BSD',
-  version='0.1dev',
+  version='0.2',
   requires=[ # FIXME does this work? docs seem inconsistent....
     'numpy(>=1.5.1)',
     'scipy'
     ],
-  packages=['eegtools', 'eegtools.io'],
+  packages=['eegtools', 'eegtools.io', 'eegtools.data'],
   )
