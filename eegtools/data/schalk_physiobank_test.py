1fad0c7785cc3a6b5c74a86d778d2982403599d7
Boris Reuderink <b.reuderink@gmail.com>
2017-01-24 21:13:11 +0100

    Remove redundant regression test for Schalk Physiobank data.

--- eegtools/data/schalk_physiobank_test.py
+++ eegtools/data/schalk_physiobank_test.py
@@ -5,10 +5,3 @@ def test_s1():
   assert str(r) == 'Recording "schalk-physiobank-s1" (64 channels x 259520 '\
     'samples) at 160.00 Hz in 14 continuous blocks, with 362 events in 11 '\
     'classes.'
-
-def test_s14():
-  '''Test subject 14 which contains runs *without* any event.'''
-  r = schalk_physiobank.load(14)
-  assert str(r) == 'Recording "schalk-physiobank-s14" (64 channels x 255520 '\
-    'samples) at 160.00 Hz in 14 continuous blocks, with 362 events in 11 '\
-    'classes.'
