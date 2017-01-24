0141527409771ff9396252eedd02c6729aa3f3d9
Boris Reuderink <b.reuderink@gmail.com>
2017-01-24 21:12:07 +0100

    Fix regression test after EDF+ annotation fix in 60753f.

--- eegtools/data/schalk_physiobank_test.py
+++ eegtools/data/schalk_physiobank_test.py
@@ -10,5 +10,5 @@ def test_s14():
   '''Test subject 14 which contains runs *without* any event.'''
   r = schalk_physiobank.load(14)
   assert str(r) == 'Recording "schalk-physiobank-s14" (64 channels x 255520 '\
-    'samples) at 160.00 Hz in 14 continuous blocks, with 361 events in 11 '\
+    'samples) at 160.00 Hz in 14 continuous blocks, with 362 events in 11 '\
     'classes.'
